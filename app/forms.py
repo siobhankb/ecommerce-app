18a2f0d2bd1127b4046a9a3a2e01fa77b9a8f323
siobhankb <siobhan.boylan@gmail.com>
2022-06-12 18:10:14 -0500

    add forms, set up templates

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email
 
-class Shopper(FlaskForm):
-    username = StringField('Username', validators=[DataRequired])
+class NewShopperForm(FlaskForm):
+    email = EmailField('Email Address', validators=[DataRequired, Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField()
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,16 @@ from flask import render_template, redirect, url_for, flash
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/edit-cart')
+def edit_cart():
+    return render_template('edit_cart.html')
+
