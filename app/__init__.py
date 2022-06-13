fc0c5ecfc8727f15bc362d7a88bf5b143dd12e23
siobhankb <siobhan.boylan@gmail.com>
2022-06-13 01:32:38 -0500

    basic file structure up

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+# from flask_login import LoginManager 
 
 app = Flask(__name__)
+
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+# login = LoginManager(app)
+
+from . import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template#, redirect, url_for, flash
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    hi = 'hello you'
+    return render_template('index.html', hi=hi)
 
 @app.route('/signup')
 def signup():
@@ -13,7 +14,19 @@ def signup():
 def login():
     return render_template('login.html')
 
+@app.route('/view-cart')
+def view_cart():
+    return render_template('view_cart.html')
+
+@app.route('/add-item')
+def add_item():
+    return render_template('add_item.html')
+
 @app.route('/edit-cart')
 def edit_cart():
     return render_template('edit_cart.html')
+
+@app.route('/delete-item')
+def delete_item():
+    return render_template('delete_item.html')
 
